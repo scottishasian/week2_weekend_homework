87b718e0d308eb3593f36426eae30db1ff3ab091
Kynan <k.song@blueyonder.co.uk>
2017-10-06 19:14:22 +0100

    Passed room capacity checks.

--- room.rb
+++ room.rb
@@ -1,13 +1,17 @@
-
+require_relative( 'songs.rb' )
 
 class Room
 
-attr_reader :name, :guests, :songs
-
+attr_reader :name, :guests, :songs, :playlist
   def initialize(name)
     @name = name
     @guests = []
     @songs = []
+    @playlist = []
+    @playlist << @songs
+
+    #Songs and artist are added into an array, which is pushed into playlist.
+
   end
 
   def guest_count
@@ -32,14 +36,35 @@ attr_reader :name, :guests, :songs
   end
 
   def show_songs
-    return @songs
+    return @playlist
   end
 
   def add_song_and_artist(song_name, artist)
-    folder = []
-    folder << song_name
-    folder << artist
-    @songs << folder
+    # folder = []
+    # folder << song_name
+    # folder << artist
+    # @songs << folder
+    @songs << song_name
+    @songs << artist
+  end
+
+  # def find_song_by_song_name(name)
+  #   for i in @playlist
+  #     for l in i
+  #       puts l[0]
+  #     end
+  #   # end
+  #
+  # end
+
+  def room_capacity
+    if @guests.count == 2
+      return "The room has #{@guests.count} people."
+    elsif @guests.count == 1
+      return "The room has #{@guests.count} person."
+    else
+      return "Empty"
+    end
   end
 
 
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -10,6 +10,7 @@ class TestRoom < MiniTest::Test
   def setup
     @room = Room.new("Singing Room")
     @guest = Guests.new("Zoe")
+    @guest2 = Guests.new("Max")
     @songs = Songs.new("Song 2", "Blur")
   end
 
@@ -40,22 +41,42 @@ class TestRoom < MiniTest::Test
 
   def test_show_songs
     result = @room.show_songs
-    assert_equal(@room.songs, result)
+    assert_equal(@room.playlist, result)
   end
 
   def test_add_song_and_artist
     @room.add_song_and_artist(@songs.song_name, @songs.artist)
-    result = @room.songs[0]
+    result = @room.songs
     array = [@songs.song_name, @songs.artist]
     assert_equal(array, result)
   end
 
   #Not too happy with this test.
 
-  def test_find_song_by_song_name
+  # def test_find_song_by_song_name
+  #   @room.add_song_and_artist(@songs.song_name, @songs.artist)
+  #   result = @room.find_song_by_song_name(@songs.song_name)
+  #   assert_equal("Song 2", result)
+  #
+  # end
+  # Come back to later.
+
+  # def test_find_song_by_artist
+  # end
+
+  def test_room_capacity_empty
+    assert_equal("Empty", @room.room_capacity)
   end
 
-  def test_find_song_by_artist
+  def test_room_capacity_one_person
+    @room.guest_check_in(@guest)
+    assert_equal("The room has 1 person.", @room.room_capacity)
+  end
+
+  def test_room_capacity_two_people
+    @room.guest_check_in(@guest)
+    @room.guest_check_in(@guest2)
+    assert_equal("The room has 2 people.", @room.room_capacity)
   end
 
 end
