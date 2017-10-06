688bb2851d509b2df53912c41d86ddd4c61eeb98
Kynan <k.song@blueyonder.co.uk>
2017-10-06 19:36:46 +0100

    Passed Test_guest_has_money.

--- specs/guests_spec.rb
+++ specs/guests_spec.rb
@@ -5,11 +5,15 @@ require( 'minitest/rg' )
 class TestGuests < MiniTest::Test
 
   def setup
-    @guests = Guests.new("Zoe")
+    @guests = Guests.new("Zoe", 1000)
   end
 
   def test_guest_has_name
     assert_equal("Zoe", @guests.name())
+  end
+
+  def test_guest_has_money
+    assert_equal(1000, @guests.money())
   end
 
 end
